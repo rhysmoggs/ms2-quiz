18b6e172cd2fe8238e345b220559bb6f2abc63ac
rhysmoggs <rhysmoggs@hotmail.com>
2022-03-12 13:58:23 +0000

    add scoring system to main.js and generate next question function

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,11 +18,24 @@ document.addEventListener("DOMContentLoaded", function() {
 // list of variables
 const gameDiv = document.getElementById('game');
 const containerDiv = document.getElementById('container');
+
 const startButton = document.getElementById('start-btn');
+
 const howToButton = document.getElementById('how-to-btn');
 const howToDiv = document.getElementById('how-container');
+
 const contactButton = document.getElementById('contact-btn');
 const contactDiv = document.getElementById('contact-container');
+
+const highscoresButton = document.getElementById('highscore-btn');
+const highscoresDiv = document.getElementById('high-container');
+const endDiv = document.getElementById('end-container');
+
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
 
 
 /*undefined let variables*/
@@ -32,6 +45,12 @@ let score = 0
 let questionCounter = 0
 let availableQuestions = []
 
+const SCORE_POINTS = 5000
+const MAX_QUESTIONS = 4
+
+/*event listener set to the start button that runs the game*/
+startButton.addEventListener("click", startGame);
+
 /*start the game*/
 startGame = () => {
     questionCounter = 0
@@ -40,4 +59,43 @@ startGame = () => {
     getNewQuestion()
     containerDiv.classList.remove('hide');
     startButton.classList.add('hide');
+    if (howToButton.style.display !== "block") {
+        howToButton.style.display = "none";
+        howToDiv.style.display = "none";
+    } else {
+        howToButton.style.display = "none";
+    }; 
+    contactButton.classList.add('hide');
+    if (highscoresButton.style.display !== "block") {
+        highscoresButton.style.display = "none";
+        highscoresDiv.style.display = "none";
+    } else {
+        highscoresButton.style.display = "none";
+    };
+};
+
+/*generates next question, if max questions reached, redirects to end.html*/
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('../end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Destination: ${questionCounter} / ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
 }
